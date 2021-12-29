58e4d078a2b31fd620156c143dad2add672eb523
vuongtungan <tunganhaihai@gmail.com>
2021-12-29 13:59:27 +0700

    (message)

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
+
+
 def store(request, category_slug=None):
     categories = None
     products = None
@@ -10,12 +13,18 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
